89210ca7593329e23bdab66e84c2fb847520a75b
ahod77 <assafhod@comcast.net>
2016-05-28 21:14:47 -0400

    DB encapsulation v2

---
+++ app/src/main/java/com/example/alonassaf/rubberducky/RubberDuckyDB2.java
@@ -0,0 +1,126 @@
+package com.example.alonassaf.rubberducky;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by AlonAssaf on 5/28/2016.
+ */
+public final class RubberDuckyDB2 {
+
+    public static final String DB_NAME = "rubberducky.db";
+    public static final int DB_VERSION = 1;
+
+    // private member
+    private static SQLiteDatabase connection;
+
+    //private constructor
+    private RubberDuckyDB2() {
+        //Prevents instantiation
+    }
+
+    public static void connect(Context context) {
+        DBHelper dbHelper = new DBHelper(context, DB_NAME, null, DB_VERSION);
+        connection = dbHelper.getWritableDatabase();
+    }
+
+    private static class DBHelper extends SQLiteOpenHelper {
+        public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Settings.initialize(db);
+
+            //db.execSQL(CREATE_ENTITY_TABLE);
+
+            // db.execSQL(CREATE_ACTIVITY_TABLE);
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.d("Rubber Ducky", "Upgrading db from version " + oldVersion + " to " + newVersion);
+
+            Settings.uninitialize(db);
+            // db.execSQL(RubberDuckyDB.DROP_ENTITY_TABLE);
+            // db.execSQL(RubberDuckyDB.DROP_ACTIVITY_TABLE);
+
+            onCreate(db);
+        }
+    }
+
+    public class Entities{
+        public class Entity{
+
+        }
+    }
+
+    public class Activities{
+        public class Activity{
+
+        }
+    }
+
+    public static class Settings{
+
+        public static final String SETTINGS_TABLE = "settings";
+        public static final String SETTINGS_ID = "id";
+        public static final int SETTINGS_ID_COL = 0;
+
+        public static final String SETTINGS_KEY = "key";
+        public static final int SETTINGS_KEY_COL = 1;
+
+        public static final String SETTINGS_VALUE = "value";
+        public static final int SETTINGS_VALUE_COL = 2;
+
+        public static final String CREATE_SETTINGS_TABLE =
+                "CREATE TABLE " + SETTINGS_TABLE + " (" +
+                        SETTINGS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        SETTINGS_KEY + " TEXT NOT NULL, " +
+                        SETTINGS_VALUE + " TEXT NOT NULL);" //Allow null?
+                ;
+
+        public static final String DROP_SETTINGS_TABLE =
+                "DROP TABLE IF EXISTS " + SETTINGS_TABLE;
+
+        public static void initialize(SQLiteDatabase _conn) {
+            _conn.execSQL(CREATE_SETTINGS_TABLE);
+        }
+
+        public static void uninitialize(SQLiteDatabase _conn) {
+            _conn.execSQL(RubberDuckyDB.DROP_SETTINGS_TABLE);
+        }
+
+        public static Setting get(String key) {
+            String where = SETTINGS_KEY + "= ?";
+            String[] whereArgs = { key };
+
+            Cursor cursor = connection.query(SETTINGS_TABLE, null, where, whereArgs, null, null, null);
+            cursor.moveToFirst();
+            if (cursor.getCount() == 0)
+                return null;
+            else {
+                Setting s = new Setting(key).setData(cursor.getString(SETTINGS_VALUE_COL));
+                cursor.close();
+                return s;
+            }
+        }
+
+        public static long set(Setting s) {
+            ContentValues cv = new ContentValues();
+            cv.put(SETTINGS_KEY, s.getKey());
+            cv.put(SETTINGS_VALUE, s.getData());
+
+            return connection.insert(SETTINGS_TABLE, null, cv);
+        }
+
+
+    }
+}
